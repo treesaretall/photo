c867cec55c4c9e39c819edf0aaff31ad1cfdab33
Matthew Hurst <matthew.hurst@alphafmc.com>
2026-09-16 10:48:21 -0400

    Use profile picture as favicon
    Sets the favicon link to the same Supabase-stored avatar used on the
    Info page, updating automatically when the picture changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react'
+import { useProfile } from './hooks/useProfile'
+import { getImageUrl } from './lib/imageUrl'
 import AppRoutes from './routes'
 
 function App() {
+  const { data: profile } = useProfile()
+
+  useEffect(() => {
+    if (!profile?.avatar_path) return
+
+    let link = document.querySelector<HTMLLinkElement>("link[rel='icon']")
+    if (!link) {
+      link = document.createElement('link')
+      link.rel = 'icon'
+      document.head.appendChild(link)
+    }
+    link.href = getImageUrl(profile.avatar_path)
+  }, [profile?.avatar_path])
+
   return <AppRoutes />
 }
 
